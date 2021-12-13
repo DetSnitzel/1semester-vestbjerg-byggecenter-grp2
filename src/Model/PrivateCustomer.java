b8f87daf85df1281616e7902ab3939a1f64a96f9
Annafalgren <10407493@ucn.dk>
2021-12-13 09:45:28 +0100

    hej

--- src/Model/PrivateCustomer.java
+++ src/Model/PrivateCustomer.java
@@ -1,27 +1,41 @@
 package Model;
 
 public class PrivateCustomer extends Person{
-	private String phone; 
-	private String CusomerID;
-	private String Discount;
-	
+	private String phone;
+	private String cusomerID;
+	private String discount;
+
+	public PrivateCustomer(String phone, String cusomerID, String discount) {
+		super();
+		this.setPhone(phone);
+		this.setCusomerID(cusomerID);
+		this.setDiscount(discount);
+	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getCusomerID() {
-		return CusomerID;
+		return cusomerID;
 	}
+
 	public void setCusomerID(String cusomerID) {
-		CusomerID = cusomerID;
+		this.cusomerID = cusomerID;
 	}
+
 	public String getDiscount() {
-		return Discount;
+		return discount;
 	}
+
 	public void setDiscount(String discount) {
-		Discount = discount;
+		this.discount = discount;
 	}
 }
+
+
  
