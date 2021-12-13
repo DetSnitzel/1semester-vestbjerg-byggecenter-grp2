09600c194c792505e4a73be2cd135a3fe1ee5e61
carolinebernat1 <10189126@ucn.dk>
2021-12-13 10:31:33 +0100

    productContainer

--- src/Model/ProductContainer.java
+++ src/Model/ProductContainer.java
@@ -1,5 +1,39 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class ProductContainer {
+	private static ProductContainer instance;
+	private ArrayList<Product> products;
+
+	private ProductContainer() {
+		products = new ArrayList<>();
+	}
+
+	public static ProductContainer getInstance() {
+		if (instance == null) {
+			instance = new ProductContainer();
+		}
+		return instance;
+	}
+
+	public void addProduct(Product product) {
+		products.add(product);
+	}
+
+	public Product findProductByBarcode(String barcode) {
+		Product product = null;
+		boolean goon = true;
+		int i = 0;
+		while (goon && i < products.size()) {
+			if (products.get(i).getBarcode().equals(barcode)) {
+				product = products.get(i);
+				goon = false;
+			}
+			i++;
+
+		}
+		return product;
+	}
 
 }
