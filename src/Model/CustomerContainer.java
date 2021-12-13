ab308867547a09d70f4042ab7250b3fced40bb5e
Annafalgren <10407493@ucn.dk>
2021-12-13 10:22:48 +0100

    customercontainer

--- src/Model/CustomerContainer.java
+++ src/Model/CustomerContainer.java
@@ -1,5 +1,37 @@
 package Model;
+import java.util.ArrayList;
+
+import model.Person;
 
 public class CustomerContainer {
-
+	private static CustomerContainer instance;
+	private ArrayList<Person> customers;
+	
+	private CustomerContainer() {
+		customers = new ArrayList<>();
+		
+	}
+	
+	public static CustomerContainer getInstance() {
+		if (instance == null) {
+			instance = new CustomerContainer();
+		}
+		return instance;
+	}
+	public Person findCustomerByPhone(String phone) {
+		Person customer = null;
+    	boolean goon = true;
+    	int i = 0;
+    	while(goon && i<customers.size()) {
+    		if(customers.get(i).getPhone().equals(phone)) {
+    			customer = customers.get(i);
+    			goon = false;
+    		}
+    		i++;
+    	}
+    	return customer;
+    }
+	public void addCustomer(Person customer) {
+    	customers.add(customer);
+	}
 }
