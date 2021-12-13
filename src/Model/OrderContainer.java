dfd4bcd8c85343d558d7ab1fbad1922033a94399
DetSnitzel <10407522@ucn.dk>
2021-12-13 10:24:52 +0100

    OrderContainer udfyldt

--- src/Model/OrderContainer.java
+++ src/Model/OrderContainer.java
@@ -1,5 +1,31 @@
 package Model;
+import java.util.ArrayList;
+
+import Model.Order;
+import Model.OrderContainer;
 
 public class OrderContainer {
+	private static OrderContainer instance;
+	private ArrayList<Order> orders;
+	
+	private OrderContainer() {
+        orders = new ArrayList<>();
+    }
+    
+    public static OrderContainer getInstance() {
+        if (instance == null) {
+            instance = new OrderContainer();
+        }
+        return instance;
+    }
+    
+    public boolean addOrder(Order order) {
+    	boolean added = true;
+    	orders.add(order);
+    	return added;
+    }
 
+    public ArrayList<Order> getList(){
+    	return orders;
+  }
 }
