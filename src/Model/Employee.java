27d21e10a102bca7b5cfb1f3af812b9767d26bda
LessIsMore42069 <10407509@ucn.dk>
2021-12-13 10:01:00 +0100

    Employee nu med super inkluderet

--- src/Model/Employee.java
+++ src/Model/Employee.java
@@ -1,7 +1,12 @@
 package Model;
 
-public class Employee {
+public class Employee extends Person {
 	
+	public Employee(String employeeRole, String employeeID, String name, String email, String postalCode, String phone, String address) {
+		super(name, phone, address, email, postalCode);
+		this.employeeRole = employeeRole;
+		this.employeeID = employeeID;
+	}
 	private String employeeRole;
 	private String employeeID;
 	
