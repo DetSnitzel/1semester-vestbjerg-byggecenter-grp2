0fab7b3f2002b9bb2e64b4752f46ee7bc0df7411
LessIsMore42069 <10407509@ucn.dk>
2021-12-14 09:33:01 +0100

    JødeMathissemand

--- src/tui/OrderTUI.java
+++ src/tui/OrderTUI.java
@@ -1,5 +1,7 @@
 package tui;
 
+import java.util.Scanner;
+
 public class OrderTUI {
 	private OrderController orderCtrl;
 	
@@ -17,12 +19,35 @@ public class OrderTUI {
 			if(choice == 1) {
 				orderMenuSelection();
 				choice = -1;
+				else if (choice == 2) {
+						System.out.println("Not yet implemented ");
+						choice = -1;
+						orderMenu();
+				}
+						else {
+							exit = true;
+				}
 			}
-		}
 		
+		
+	}
+	private void writeOrderMenu() {
+		Scanner keyboard = new Scanner(System.in);
+		
+	}
+	private int inputIntFromScanner() {
+		//Return user input as an int
+		Scanner keyboard = new Scanner (System.in);
+		while(!keyboard.hasNextInt()) {
+		System.out.println("input has to be a number, try again");
+		keyboard.nextLine();
+		}
+		return keyboard.nextInt();
 	}
 	private void orderMenuSelection() {
 		// TODO Auto-generated method stub
+		
+		
 		
 	}
 	
