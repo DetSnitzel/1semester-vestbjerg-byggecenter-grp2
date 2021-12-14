05bbc538d15a53fc948cc351cccb73b30ba60422
carolinebernat1 <10189126@ucn.dk>
2021-12-14 12:41:29 +0100

    Update OrderTUI

--- src/tui/OrderTUI.java
+++ src/tui/OrderTUI.java
@@ -1,58 +1,46 @@
 package tui;
 
-
-
 import java.util.Scanner;
 import controller.OrderController;
+import model.PrivateCustomer;
 
 public class OrderTUI {
-	private OrderController orderCtrl;
+	private OrderController orderController;
 
 	public OrderTUI() {
-		orderCtrl = new OrderController();
+		orderController = new OrderController();
 	}
 
 	public void start() {
 		System.out.println("Order TUI: ");
 
-		orderMenu();
-	}
+		boolean running = true;
 
-	private void orderMenu() {
-		boolean exit = false;
-	
-		
-		while (!exit) {
-			int choice = writeOrderMenu();
-			switch (choice) {
+		while (running) {
+			int selection = writeOrderMenu();
+			switch (selection) {
 			case 1:
-				orderCtrl.createOrder();
-				System.out.println("Order is created: " + orderCtrl.getOrderNo());
+				orderController.createOrder();
+				System.out.println("Order is created. Now find customer by phone");
 				
-				boolean addCustomer = false;
-				while (addCustomer == false && exit == false) {
+				boolean addPrivateCustomer = false;
+				while (addPrivateCustomer == false && running == true) {
 					System.out.println("Enter phone number of customer");
 					Scanner scanCustomer = new Scanner(System.in);
 					String phone = scanCustomer.nextLine();
-					orderCtrl.addCustomer(phone);
-					PrivateCustomer pc = orderCtrl.addCustomer(phone);
-					
+					orderController.addCustomer(phone);
+					PrivateCustomer pc = orderController.addCustomer(phone);
+					if (pc != null) {
+						System.out.println(pc.getName() + ": was added to order");
+						addPrivateCustomer = true;
+					}
 				}
-			
-			
-				orderMenuSelection();
-				choice = -1;
-			} else if (choice == 2) {
-				System.out.println("Not yet implemented ");
-				choice = -1;
-				orderMenu();
-			} else {
-				exit = true;
 			}
 		}
 	}
-
-	private void writeOrderMenu() {
+				
+			
+	private int writeOrderMenu() {
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println();
 		System.out.println("*** Order Menu ***");
@@ -62,56 +50,51 @@ public class OrderTUI {
 		System.out.println(" (0) Previous menu");
 		System.out.print("Choose an option: ");
 		System.out.println();
-	}
-
-	private void findCustomerMenu() {
-		System.out.println();
-		System.out.println("Search after: ");
-		System.out.println("Options: 0, 1");
-		System.out.println("(1) Search phoneNo");
-		System.out.println("(0) Previous menu");
-
-		int x = inputIntFromScanner();
-		if (x == 1) {
-			System.out.println("The persons phonenumber :  ");
-			String phone = userPhoneNumber();
-			phoneNumberFromUser(phone);
-			findCustomer(phone);
-		} else if (x == 0) {
-			orderMenuSelection();
-		}
-	}
-
-	private String userPhoneNumber() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private void phoneNumberFromUser(String phone) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void findCustomer(String phone) {
-		if (orderCtrl.findCustomerByPhone(phone)) {
-
-		}
-
-	}
-
-	private int inputIntFromScanner() {
-		// Return user input as an int
-		Scanner keyboard = new Scanner(System.in);
+		
 		while (!keyboard.hasNextInt()) {
-			System.out.println("input has to be a number, try again");
+			System.out.println("Input must be a number");
 			keyboard.nextLine();
 		}
-		return keyboard.nextInt();
+		int selection = keyboard.nextInt();
+		return selection;
+		
 	}
-
-	private void orderMenuSelection() {
-		// TODO Auto-generated method stub
-
-	}
-
 }
+
+				
+
+//	private void findCustomerMenu() {
+//		System.out.println();
+//		System.out.println("Search after: ");
+//		System.out.println("Options: 0, 1");
+//		System.out.println("(1) Search phoneNo");
+//		System.out.println("(0) Previous menu");
+//
+//		int x = inputIntFromScanner();
+//		if (x == 1) {
+//			System.out.println("The persons phonenumber :  ");
+//			String phone = userPhoneNumber();
+//			phoneNumberFromUser(phone);
+//			findCustomer(phone);
+//		} else if (x == 0) {
+//			writeOrderMenu();
+//		}
+//	}
+
+
+//	private int inputIntFromScanner() {
+//		// Return user input as an int
+//		Scanner keyboard = new Scanner(System.in);
+//		while (!keyboard.hasNextInt()) {
+//			System.out.println("input has to be a number, try again");
+//			keyboard.nextLine();
+//		}
+//		return keyboard.nextInt();
+//	}
+//
+//	private void orderMenuSelection() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
