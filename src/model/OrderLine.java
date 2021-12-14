8e3bd2666c53fe9068e65e170f95f9121462ce6b
DetSnitzel <10407522@ucn.dk>
2021-12-14 09:30:20 +0100

    OrderLine done

--- src/model/OrderLine.java
+++ src/model/OrderLine.java
@@ -1,5 +1,37 @@
 package model;
 
 public class OrderLine {
+	private int quantity;
+	private double subtotal;
+	private Product product;
+	
+	public OrderLine(Product product, int quantity) {
+		this.product = product;
+		this.quantity = quantity;
+		this.subtotal = product.getPrice() * quantity;
+	}
 
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
 }
